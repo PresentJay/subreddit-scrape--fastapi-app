f57b668c57134b59eeade9b89d342ad932943359
Present_Jay.Dev <Presentj94@gmail.com>
2023-12-14 15:50:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import json
-import logging
 import random
 import requests
 from flask import Flask, send_file
@@ -9,46 +7,43 @@ from time import sleep
 
 app = Flask(__name__)
 
-# Constants
 SUBREDDIT_NAME = "ProgrammerHumor"
 FETCHING_AMOUNT = 99
 
-# Logging configuration
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 def get_image_url():
-    duration = 5
+    retries = 5
     url = f"https://www.reddit.com/r/{SUBREDDIT_NAME}.json?limit={FETCHING_AMOUNT}"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}
-    
-    while duration >= 1:
-        req = requests.get(url=url, headers=headers)
+
+    while retries > 0:
+        response = requests.get(url=url, headers=headers)
+
+        if response.status_code == 404:
+            app.logger.warning("Received 404 error. Retrying...")
+            retries -= 1
+            sleep(random.random())
+            continue
+
         try:
-            json_data = req.json()
-        except json.decoder.JSONDecodeError:
-            logger.warning("Invalid JSON received. Retrying...")
-            duration -= 1
+            json_data = response.json()
+        except json.decoder.JSONDecodeError as e:
+            app.logger.warning(f"Invalid JSON received. Error: {e}. Retrying...")
+            retries -= 1
             sleep(random.random())
             continue
 
-        if "error" in json_data.get("data", {}).get("children", []):
-            logger.warning("Error in response. Retrying...")
-            duration -= 1
+        children = json_data.get("data", {}).get("children", [])
+        if not children:
+            app.logger.warning("No image URLs found. Retrying...")
+            retries -= 1
             sleep(random.random())
             continue
 
-        img_url_list = json_data.get("data", {}).get("children", [])
-        
-        if not img_url_list:
-            logger.warning("No image URLs found. Retrying...")
-            duration -= 1
-            sleep(random.random())
-            continue
-
-        selected = random.choice(img_url_list)
+        selected = random.choice(children)
         if selected.get("data", {}).get("post_hint") == "image":
-            return selected["data"]["url"]
+            img_url = selected["data"]["url"]
+            app.logger.info(f"Selected image URL: {img_url}")
+            return img_url
 
     return None
 
@@ -69,22 +64,16 @@ def set_response_headers(response):
 def return_meme():
     img_url = get_image_url()
 
-    if img_url is None:
-        # Handle the case where no valid image URL is found
-        return "No valid image found", 404  # You can customize this response as needed
+    if img_url:
+        response = requests.get(img_url, stream=True)
 
-    res = requests.get(img_url, stream=True)
+        if response.status_code == 200:
+            content_type = response.raw.headers["Content-Type"]
+            response.raw.decode_content = True
+            img = Image.open(response.raw)
+            return serve_pil_image(img, content_type)
 
-    if res.status_code != 200:
-        # Handle the case where the image request is unsuccessful
-        return f"Error fetching image: {res.status_code}", 500  # You can customize this response as needed
-
-    # set MIME type as scraped image MIME (jpeg, png, gif ...)
-    content_type = res.headers.get("Content-Type", "image/jpeg")
-
-    res.raw.decode_content = True
-    img = Image.open(res.raw)
-    return serve_pil_image(img, content_type)
+    return "Error: Unable to fetch a valid image", 500
 
 if __name__ == "__main__":
     app.run(debug=True)
