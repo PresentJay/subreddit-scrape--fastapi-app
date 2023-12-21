82c21b96a7cff0758f4c111ccceb0390392d433b
Present_Jay.Dev <Presentj94@gmail.com>
2023-12-21 14:57:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,18 @@ def getImgURL():
     duration = 5
     
     url = f"https://www.reddit.com/r/{subredditName}.json?limit={fetchingAmount}"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Upgrade-Insecure-Requests': 1,
+        'Dnt': 1,
+        'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': 'macOS',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1'
+    }
     req = requests.get(url=url, headers=headers)
     if req.status_code == 200:
         try:
